029f40558071e1359c266ad9704d4947f2d0e57f
Scott Rice <meris608@gmail.com>
2011-08-29 13:02:35 -0700

    Made the docs page look like the presentations page

--- writer.php
+++ writer.php
@@ -42,39 +42,106 @@ $page_title = 'Docs';
 $recentFiles = getRecentFiles($user_id, 6, 10);
 require_once('core/template/head/header.php');
 ?>
-
-<div id="home-left">
-    <h1>Recent Documents</h1>
-
+<script>
+setvar = false;
+function openDoc(did) {
+  if (setvar == false) {
+    window.location = '/app/writer.php?n=2&doc_id=' + did;
+  } else {
+    setvar = false;
+  }
+}
+$('.chooseClassBox').click(function() {
+  setvar = true;
+});
+</script>
+<style type="text/css">
+.lecButton {
+  height:40px;border-bottom:1px solid #ccc;padding-left:5px;cursor: pointer;margin-top:5px
+}
+.lecButton:hover {
+    opacity:0.75;
+  filter:alpha(opacity=75);
+}
+.genButBkg:hover {
+  -moz-box-shadow: 0 0 2px #333;
+  -webkit-box-shadow: 0 0 2px #333;
+  box-shadow: 0 0 2px #333;
+}
+.lecEl {
+  height:40px;
+  border:1px solid #ccc;
+  padding:10px;
+  cursor: pointer;
+  margin-top:10px;
+}
+.lecEl:hover {
+  background: #f4f4f4;
+}
+.lecButAct {
+  background-color: #fff;
+}
+.chooseClassBox {
+position:absolute; width:200px;  border:1px solid #999; background:#fff; display:none;
+margin-top:5px;
+margin-left:-65px;
+cursor:default;
+}
+.cLister {
+  font-size:13px;font-weight:bolder;padding:10px;
+  cursor:pointer;
+  border-top:1px solid #ccc;
+}
+.cLister:hover {
+  background:#e1e1e1;
+}
+.hostLecBut {
+  border:1px solid #999;
+  font-size:13px;margin-top:5px;width:125px;padding:5px
+}
+</style>
 <?php
-foreach ($recentFiles as $file) {
-    echo '<div style="height:40px;border-bottom:1px solid #ccc;padding-left:5px;cursor: pointer;" onClick="window.location = \'writer.php?n=2&doc_id=' . $file['id'] . '\';">
-       <span style="font-size:13px">' . $file['name'] . '</span><br />
-           <span style="font-size:12px; color:#999">' . date("g:ia F jS, Y", strtotime($file['time_date'])) . '</span><br />
-            </div>';
-}
-
-if(empty($recentFiles)){
-    echo '<p style="color:#999">No recent documents found</p>';
-}
+//  TODO: Mark a difference between opening a doc and converting a word/openoffice doc
 ?>
+<div id="home-left" style="-moz-box-shadow:inset -4px 4px 10px -4px #ccc;
+-webkit-box-shadow:inset -4px 4px 10px -4px #ccc;
+box-shadow:inset -4px 4px 10px -4px #ccc;">
+<h1 style="-moz-box-shadow:inset -4px 4px 10px -4px #ccc;
+-webkit-box-shadow:inset -4px 4px 10px -4px #ccc;
+box-shadow:inset -4px 4px 4px -3px #ccc;">Get started with Docs</h1>
+  <div class="lecButton" onClick="openBox('writer.php?n=1', 350); return false">
+    <img src="<?php echo $imgServer; ?>gen/addDoc.png" style="width:32px;float:left;margin-right:8px" />
+    <div style="font-size:13px">Create Document</div>
+    <div style="font-size:12px; color:#999; margin-top:-3px">Create a new document.</div>
+  </div>
 
+  <div class="lecButton" onClick="openBox('writer.php?n=5', 350); return false">
+    <img src="<?php echo $imgServer; ?>gen/opendoc.png" style="width:32px;float:left;margin-right:8px" />
+    <div style="font-size:13px">Open Document</div>
+    <div style="font-size:12px; color:#999; margin-top:-3px">Open an existing document.</div>
+  </div>
+
+  <!-- <div class="lecButton" onClick="openBox('writer.php?n=4', 350); return false">
+    <img src="<?php echo $imgServer; ?>gen/convdoc.png" style="width:32px;float:left;margin-right:8px" />
+    <div style="font-size:13px">Import from Word</div>
+    <div style="font-size:12px; color:#999; margin-top:-3px">Converts .doc, .docx, or .odt</div>
+  </div> -->
 </div>
-
 <div style="float:right; width:670px">
-<div  style="font-size:22px; color:#666; margin-bottom:15px">Welcome To Docs</div>
-    <div id="createDoc" style="width:300px; float:left" class="colorswap fullRound" onClick="openBox('writer.php?n=1', 350); return false">
-<span style="font-size:18px">Create Document</span><br />
-<span style="color:#999; font-size:13px">Start a brand new document using Docs. You can edit, view, print and download this document from any internet enabled computer or device.</span>
+  <div style="font-size:22px; color:#666; margin-bottom:15px">Recent documents</div>
+  <?php
+    foreach ($recentFiles as $file) {
+      echo '<div class="lecEl fullRound" onClick="openDoc(' . $file['id'] . ');">';
+      echo '<img src="' . $imgServer . 'gen/document.png" style="float:left;width:40px;margin-right:10px" /><span style="font-size:14px">' . $file['name'] . '</span><br />
+        <span style="font-size:11px; color:#999">' . date("g:ia F jS, Y", strtotime($file['time_date'])) . '</span><br />
+      </div>';
+    }
 
-    </div>
-
-<div id="openDoc" style="width:300px; float:right" class="colorswap fullRound" onClick="openBox('writer.php?n=5', 350); return false">
-<span style="font-size:18px">Open Document</span><br />
-<span style="color:#999; font-size:13px">Have an existing document? Our software can open ClassConnect documents as well as a slew of other file types including .doc, .docx, .odt,  and more!</span>
-
-</div>
-    
+    if(empty($recentFiles)){
+      echo '<p style="color:#999;font-size:16px;text-align:center">You don\'t have any recent lectures!<br />Create a new lecture or import a PowerPoint to get started!</p>';
+    }
+  ?>
+  </div>
 </div>
 <?php
 require_once('core/template/foot/footer.php');
